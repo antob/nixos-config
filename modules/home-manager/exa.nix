0533dc35d108cee6a5f034f5c5feb362c4ab1769
Tobias Lindholm <tobias.lindholm@antob.se>
2023-03-07 19:18:11 +0100

    Fix exa.

--- modules/home-manager/exa.nix
+++ modules/home-manager/exa.nix
@@ -8,17 +8,16 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs.exa = {
-      enable = true;
-      enableAliases = true;
-      git = true;
-      extraOptions = [ "--group-directories-first" "--header" ];
-    };
+    programs.exa = { enable = true; };
 
     programs.zsh.shellAliases =
       let treeIgnore = "'cache|log|logs|node_modules|vendor|.git'";
       in {
-        # lt = "ls --tree -D -L 2 -I ${treeIgnore}";
+        ls = "exa --group-directories-first";
+        la = "ls -a";
+        l = "ls --git -l";
+        ll = "ls --git -al";
+        lt = "ls --tree -D -L 2 -I ${treeIgnore}";
         ltt = "ls --tree -D -L 3 -I ${treeIgnore}";
         lttt = "ls --tree -D -L 4 -I ${treeIgnore}";
         ltttt = "ls --tree -D -L 5 -I ${treeIgnore}";
