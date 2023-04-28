c0ede9e5b578a1f096931bcceff417f0cd4d9a70
Tobias Lindholm <tobias.lindholm@antob.se>
2023-04-28 07:33:12 +0200

    feat: add Teams module

---
+++ modules/apps/teams/default.nix
@@ -0,0 +1,17 @@
+{ options, config, pkgs, lib, ... }:
+
+with lib;
+let cfg = config.antob.apps.teams;
+in {
+  options.antob.apps.teams = with types; {
+    enable = mkEnableOption "Enable Teams";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ teams ];
+    antob.persistence.home.directories = [
+      ".config/Microsoft/Microsoft Teams"
+      ".config/Microsoft Teams - Preview"
+    ];
+  };
+}
