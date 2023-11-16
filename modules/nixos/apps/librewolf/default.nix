38b463b766b4b0838e2e3f2905aab94ac58cce07
Tobias Lindholm <tobias.lindholm@antob.se>
2023-11-16 21:29:17 +0100

    feat: update librewolf config

--- modules/nixos/apps/librewolf/default.nix
+++ modules/nixos/apps/librewolf/default.nix
@@ -19,6 +19,23 @@ in {
           "privacy.sanitize.sanitizeOnShutdown" = false;
           "webgl.disabled" = false;
           "media.ffmpeg.vaapi.enabled" = true;
+
+          "general.smoothScroll.lines.durationMaxMS" = 125;
+          "general.smoothScroll.lines.durationMinMS" = 125;
+          "general.smoothScroll.mouseWheel.durationMaxMS" = 200;
+          "general.smoothScroll.mouseWheel.durationMinMS" = 100;
+          "general.smoothScroll.msdPhysics.enabled" = true;
+          "general.smoothScroll.other.durationMaxMS" = 125;
+          "general.smoothScroll.other.durationMinMS" = 125;
+          "general.smoothScroll.pages.durationMaxMS" = 125;
+          "general.smoothScroll.pages.durationMinMS" = 125;
+          "layers.acceleration.force-enabled" = true;
+          "mousewheel.min_line_scroll_amount" = 30;
+          "mousewheel.system_scroll_override_on_root_content.enabled" = true;
+          "mousewheel.system_scroll_override_on_root_content.horizontal.factor" = 175;
+          "mousewheel.system_scroll_override_on_root_content.vertical.factor" = 175;
+          "toolkit.scrollbox.horizontalScrollDistance" = 6;
+          "toolkit.scrollbox.verticalScrollDistance" = 6;
         };
       };
 
