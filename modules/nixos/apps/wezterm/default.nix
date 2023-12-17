747484b1c00d06d39f2582fe6fdc9e81eb4bfe3c
Tobias Lindholm <tobias.lindholm@antob.se>
2023-12-17 13:15:49 +0100

    feat: add basic wezterm module

---
+++ modules/nixos/apps/wezterm/default.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+
+with lib;
+with lib.antob;
+let cfg = config.antob.apps.wezterm;
+in {
+  options.antob.apps.wezterm = with types; {
+    enable = mkEnableOption "Enable wezterm";
+  };
+
+  config = mkIf cfg.enable {
+    antob.home.extraOptions.programs.wezterm = {
+      enable = true;
+      enableZshIntegration = true;
+      extraConfig = ''
+        return {
+          color_scheme = "OneDark (base16)",
+          font = wezterm.font "Hack Nerd Font",
+          font_size = 12.0,
+          window_decorations = "NONE",
+        }
+      '';
+    };
+  };
+}
