7b5f313b8f82bf7d316a3621d00ce0b38f3fe9e7
Tobias Lindholm <tobias.lindholm@antob.se>
2024-01-15 19:43:31 +0100

    feat: add docker module. fix podman dns.

---
+++ modules/nixos/virtualisation/docker/default.nix
@@ -0,0 +1,24 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.antob;
+let cfg = config.antob.virtualisation.docker;
+in {
+  options.antob.virtualisation.docker = with types; {
+    enable = mkEnableOption "Whether or not to enable docker.";
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker = {
+      enable = true;
+      storageDriver = "btrfs";
+    };
+
+    antob = {
+      user.extraGroups = [ "docker" ];
+      tools.zsh.extraOhMyZshPlugins = [ "docker-compose" ];
+    };
+
+    environment.systemPackages = with pkgs; [ docker-compose ];
+  };
+}
--- modules/nixos/virtualisation/podman/default.nix
+++ modules/nixos/virtualisation/podman/default.nix
@@ -13,6 +13,8 @@ in {
       enable = true;
       dockerCompat = true;
       dockerSocket.enable = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
     };
 
     virtualisation.containers.storage.settings = {
