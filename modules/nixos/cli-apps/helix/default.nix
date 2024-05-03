2af70914ea81e13f093b6765034d20614dd15b84
Tobias Lindholm <tobias.lindholm@antob.se>
2024-05-03 09:08:05 +0200

    feat: update helix module

--- modules/nixos/cli-apps/helix/default.nix
+++ modules/nixos/cli-apps/helix/default.nix
@@ -20,6 +20,7 @@ in {
             mouse = false;
             cursorline = true;
             bufferline = "multiple";
+            auto-format = true;
             color-modes = true;
             shell = [ "/usr/bin/zsh" "-c" ];
 
@@ -56,9 +57,23 @@ in {
               C-q = ":qa";
               C-u = [ "half_page_up" "goto_window_center" ];
               C-d = [ "half_page_down" "goto_window_center" ];
+              C-g = [ ":new" ":insert-output lazygit" ":buffer-close!" ":redraw" ];
+              "A-[" = "goto_previous_buffer";
+              "A-]" = "goto_next_buffer";
+              A-w = ":buffer-close";
+              y = [ ":clipboard-yank" "yank" ];
+              X = [ "extend_line_up" "extend_to_line_bounds" ];
+              space = {
+                c = "file_picker_in_current_buffer_directory";
+              };
             };
 
             insert = { j = { k = "normal_mode"; }; };
+
+            select = {
+              y = [ ":clipboard-yank" "yank" ];
+              X = [ "extend_line_up" "extend_to_line_bounds" ];
+            };
           };
         };
 
