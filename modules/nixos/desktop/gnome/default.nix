b987302b078164675184bb33a4af110209cc8d7a
Tobias Lindholm <tobias.lindholm@antob.se>
2024-11-29 23:02:37 +0100

    fix: fix gnome custom keybindings

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -331,6 +331,18 @@ in
           "org/gnome/settings-daemon/plugins/power" = {
             ambient-enabled = false;
           };
+          # Reserve custom keybinding locations
+          "org/gnome/settings-daemon/plugins/media-keys" = {
+            custom-keybindings = [
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/"
+            ];
+          };
+          # Set custom keybindings
           "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
             binding = "<Super>w";
             command = "firefox";
