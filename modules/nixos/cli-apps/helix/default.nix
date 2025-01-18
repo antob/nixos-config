c74b1c2c5ef9fb89a82f04953541107f9a9f3911
Tobias Lindholm <tobias.lindholm@antob.se>
2025-01-18 12:34:19 +0100

    feat: change helix theme and lsp

--- modules/nixos/cli-apps/helix/default.nix
+++ modules/nixos/cli-apps/helix/default.nix
@@ -1,9 +1,15 @@
-{ options, config, lib, ... }:
+{
+  config,
+  lib,
+  ...
+}:
 
 with lib;
 with lib.antob;
-let cfg = config.antob.cli-apps.helix;
-in {
+let
+  cfg = config.antob.cli-apps.helix;
+in
+{
   options.antob.cli-apps.helix = with types; {
     enable = mkEnableOption "Whether or not to enable helix.";
   };
@@ -13,7 +19,7 @@ in {
       programs.helix = {
         enable = true;
         settings = {
-          theme = "onedark";
+          theme = "catppuccin_mocha";
 
           editor = {
             line-number = "relative";
@@ -22,7 +28,10 @@ in {
             bufferline = "multiple";
             auto-format = true;
             color-modes = true;
-            shell = [ "/usr/bin/zsh" "-c" ];
+            shell = [
+              "/usr/bin/zsh"
+              "-c"
+            ];
 
             cursor-shape = {
               insert = "bar";
@@ -55,24 +64,51 @@ in {
           keys = {
             normal = {
               C-q = ":qa";
-              C-u = [ "half_page_up" "goto_window_center" ];
-              C-d = [ "half_page_down" "goto_window_center" ];
-              C-g = [ ":new" ":insert-output lazygit" ":buffer-close!" ":redraw" ];
+              C-u = [
+                "half_page_up"
+                "goto_window_center"
+              ];
+              C-d = [
+                "half_page_down"
+                "goto_window_center"
+              ];
+              C-g = [
+                ":new"
+                ":insert-output lazygit"
+                ":buffer-close!"
+                ":redraw"
+              ];
               "A-[" = "goto_previous_buffer";
               "A-]" = "goto_next_buffer";
               A-w = ":buffer-close";
-              y = [ ":clipboard-yank" "yank" ];
-              X = [ "extend_line_up" "extend_to_line_bounds" ];
+              y = [
+                ":clipboard-yank"
+                "yank"
+              ];
+              X = [
+                "extend_line_up"
+                "extend_to_line_bounds"
+              ];
               space = {
                 c = "file_picker_in_current_buffer_directory";
               };
             };
 
-            insert = { j = { k = "normal_mode"; }; };
+            insert = {
+              j = {
+                k = "normal_mode";
+              };
+            };
 
             select = {
-              y = [ ":clipboard-yank" "yank" ];
-              X = [ "extend_line_up" "extend_to_line_bounds" ];
+              y = [
+                ":clipboard-yank"
+                "yank"
+              ];
+              X = [
+                "extend_line_up"
+                "extend_to_line_bounds"
+              ];
             };
           };
         };
@@ -83,6 +119,9 @@ in {
               diagnostics = true;
               formatting = true;
             };
+            ruby-lsp = {
+              command = "ruby-lsp";
+            };
           };
 
           language = [
@@ -91,10 +130,15 @@ in {
               auto-format = true;
               formatter = {
                 command = "standardrb";
-                args = [ "--stdin" "foo.rb" "--fix" "--stderr" ];
+                args = [
+                  "--stdin"
+                  "foo.rb"
+                  "--fix"
+                  "--stderr"
+                ];
               };
               language-servers = [
-                { name = "solargraph"; }
+                { name = "ruby-lsp"; }
               ];
             }
             {
