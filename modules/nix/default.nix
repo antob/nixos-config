e09eff27ec5440860dbc2e4115ddb09061c24715
Tobias Lindholm <tobias.lindholm@antob.se>
2025-07-10 21:10:41 +0200

    feat: update nix config

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -12,29 +12,38 @@
     };
   };
 
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    http-connections = 50;
-    warn-dirty = false;
-    log-lines = 50;
-    sandbox = "relaxed";
-    auto-optimise-store = true;
-    trusted-users = [
-      "root"
-      config.antob.user.name
-    ];
-    allowed-users = [
-      "root"
-      config.antob.user.name
-    ];
-    substituters = [
-      "https://cache.nixos.org/"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    ];
+  nix = {
+    # Remove nix-channel related tools & configs, we use flakes instead.
+    channel.enable = false;
+
+    settings = {
+      experimental-features = "nix-command flakes";
+      http-connections = 50;
+      warn-dirty = false;
+      log-lines = 50;
+      sandbox = "relaxed";
+      auto-optimise-store = true;
+      trusted-users = [
+        "root"
+        config.antob.user.name
+      ];
+      allowed-users = [
+        "root"
+        config.antob.user.name
+      ];
+      substituters = [
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+    };
   };
 
   # Enable nixos-rebuild-ng, a full rewrite of nixos-rebuild in Python. Available for testing.
   system.rebuild.enableNg = true;
+
+  antob.persistence.home.directories = [
+    ".local/share/nix"
+  ];
 }
