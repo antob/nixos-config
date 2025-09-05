90b016ba39975e4aeca6d178ab655807c6ded12b
Tobias Lindholm <tobias.lindholm@antob.se>
2025-09-05 18:51:57 +0200

    fix: remove unsupported nvim-tree options

--- modules/cli-apps/neovim/plugins/nvim-tree.nix
+++ modules/cli-apps/neovim/plugins/nvim-tree.nix
@@ -3,32 +3,17 @@
     nvim-tree = {
       enable = true;
       settings = {
-        disableNetrw = true;
-        hijackCursor = true;
-        syncRootWithCwd = true;
-
         filters.dotfiles = false;
-        updateFocusedFile = {
-          enable = true;
-          updateRoot = false;
-        };
         view = {
           width = 40;
-          preserveWindowProportions = true;
         };
         renderer = {
-          rootFolderLabel = false;
-          highlightGit = true;
-          indentMarkers = {
-            enable = true;
-          };
           icons = {
             glyphs = {
               default = "󰈚";
               folder = {
                 default = "";
                 empty = "";
-                emptyOpen = "";
                 open = "";
                 symlink = "";
               };
