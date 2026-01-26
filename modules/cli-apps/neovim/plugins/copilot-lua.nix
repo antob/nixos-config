af15d3d4203a0b45e4ec213d9dfef02983c04126
Tobias Lindholm <tobias.lindholm@antob.se>
2026-01-26 14:05:13 +0100

    fix: shortcut to hide copilot suggestion on C-c

--- modules/cli-apps/neovim/plugins/copilot-lua.nix
+++ modules/cli-apps/neovim/plugins/copilot-lua.nix
@@ -45,9 +45,9 @@
       vim.keymap.set("i", "<C-c>", function()
         if copilot_suggestion.is_visible() then
           copilot_suggestion.dismiss()
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
         else
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
         end
       end, { noremap = true, silent = true, desc = "Dismiss Copilot suggestion and exit insert mode" })
     '';
