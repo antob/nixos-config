ea64f1486cf0406953e5dccc20f4ca9b628ba90d
Tobias Lindholm <tobias.lindholm@antob.se>
2026-01-28 20:12:37 +0100

    chore: update music-assistant version

--- hosts/hyllan/music-assistant.nix
+++ hosts/hyllan/music-assistant.nix
@@ -9,7 +9,7 @@ in
 {
   virtualisation.oci-containers.containers = {
     mass = {
-      image = "ghcr.io/music-assistant/server:2.8.0b7";
+      image = "ghcr.io/music-assistant/server:2.8.0b8";
       autoStart = true;
       extraOptions = [
         "--network=host"
