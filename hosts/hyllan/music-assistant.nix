d714f965f4d4999476b5cccd84658b925358c8c2
Tobias Lindholm <tobias.lindholm@antob.se>
2026-02-12 18:36:04 +0100

    feat: bump music-assistant image version

--- hosts/hyllan/music-assistant.nix
+++ hosts/hyllan/music-assistant.nix
@@ -9,7 +9,7 @@ in
 {
   virtualisation.oci-containers.containers = {
     mass = {
-      image = "ghcr.io/music-assistant/server:2.8.0b8";
+      image = "ghcr.io/music-assistant/server:2.8.0b12";
       autoStart = true;
       extraOptions = [
         "--network=host"
