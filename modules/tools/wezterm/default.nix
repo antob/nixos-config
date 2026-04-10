f47c3477e1eca847844a5c0ad816d8bef074c3a3
Tobias Lindholm <tobias.lindholm@antob.se>
2026-04-10 16:38:41 +0200

    feat: update wezterm config

--- modules/tools/wezterm/default.nix
+++ modules/tools/wezterm/default.nix
@@ -29,11 +29,26 @@ in
 
         -- Plugin imports
         -- Run the command `wezterm.plugin.update_all()` from the debug overlay to update all local plugins.
-        local ws_mgr = wezterm.plugin.require("https://github.com/antob/wezterm-workspace-manager")
+        -- local ws_mgr = wezterm.plugin.require("https://github.com/antob/wezterm-workspace-manager")
 
         -- Customize the Tokyo Night theme
         local custom_theme = wezterm.color.get_builtin_schemes()["Tokyo Night"]
-        custom_theme.selection_bg = "#${colors.base04}"
+        -- custom_theme.selection_bg = "#${colors.base04}"
+        custom_theme.selection_fg = "none"
+        custom_theme.selection_bg = "rgba(90% 90% 90% 25%)"
+        custom_theme.split = "#${colors.base0C}"
+        config.inactive_pane_hsb = {
+          saturation = 0.8,
+          brightness = 0.65,
+        }
+
+        -- Helper functions
+        function basename(s)
+          return string.gsub(s, "(.*[/\\])(.*)", "%2")
+        end
+        function projects(rel_dir)
+          return wezterm.home_dir .. "/Projects/" .. rel_dir
+        end
 
         -- General settings
         config.color_schemes = {
@@ -56,19 +71,7 @@ in
         config.cursor_blink_rate = 0
         config.command_palette_bg_color = "#${colors.base13}"
         config.command_palette_fg_color = "#${colors.base06}"
-
-        -- Multiplexing
-        config.unix_domains = {
-          {
-            name = "unix",
-          },
-        }
-
-        -- This causes `wezterm` to act as though it was started as
-        -- `wezterm connect unix` by default, connecting to the unix
-        -- domain on startup.
-        -- If you prefer to connect manually, leave out this line.
-        config.default_gui_startup_args = { "connect", "unix" }
+        config.window_close_confirmation = "NeverPrompt"
 
         -- Tab bar settings
         config.enable_tab_bar = true
@@ -86,9 +89,96 @@ in
           window:set_right_status(window:active_workspace())
         end)
 
+        function last_path_segment(path)
+          return path:match("([^/]+)$")
+        end
+
+        function spawn_window(cwd, title, domain)
+          title = title or last_path_segment(cwd)
+          local tab, pane, window = wezterm.mux.spawn_window({
+            workspace = title,
+            cwd = cwd,
+          })
+          tab:set_title(title)
+
+          return window, tab, pane
+        end
+
+        function spawn_or_setup_tab(window_or_tab, cwd, title)
+          title = title or last_path_segment(cwd)
+          local tab = nil
+          local pane = nil
+
+          if type(window_or_tab.spawn_tab) == "function" then
+            tab, pane, _ = window_or_tab:spawn_tab({
+              cwd = cwd,
+            })
+          else
+            tab, pane = window_or_tab, window_or_tab:active_pane()
+          end
+
+          tab:set_title(title)
+
+          return tab, pane
+        end
+
+        function two_splits_pane(pane)
+          pane:split({ direction = "Left", size = 0.5 })
+        end
+
+        function three_splits_pane(pane)
+          pane:split({ direction = "Bottom", size = 0.5 })
+          pane:split({ direction = "Left", size = 0.33, top_level = true })
+        end
+
+        function two_splits_tab(window_or_tab, cwd, title)
+          local tab, pane = spawn_or_setup_tab(window_or_tab, cwd, title)
+          two_splits_pane(pane)
+
+          return tab, pane
+        end
+
+        function three_splits_tab(window_or_tab, cwd, title)
+          local tab, pane = spawn_or_setup_tab(window_or_tab, cwd, title)
+          three_splits_pane(pane)
+
+          return tab, pane
+        end
+
+        -- Default workspaces
+        wezterm.on("gui-startup", function()
+          -- nixos-config
+          local window, tab, pane = spawn_window(projects("nixos-config"))
+          two_splits_pane(pane)
+
+          -- trafikportalen
+          local window, tab, pane = spawn_window(projects("hl-design/trafikportal"))
+          three_splits_pane(pane)
+
+          -- vesselcare
+          local cwd = projects("obit/VesselCare")
+          local window, tab, pane = spawn_window(cwd .. "/VesselCare-webapp", "VesselCare")
+          three_splits_tab(tab, cwd, "webapp")
+          three_splits_tab(window, cwd .. "/AnchorPoint")
+          three_splits_tab(window, cwd .. "/ProcessingPipeline")
+          three_splits_tab(window, cwd .. "/UserAccessManagement")
+          two_splits_tab(window, cwd .. "/internal-python-library", "apilib")
+          tab:activate()
+
+          -- default
+          spawn_window(wezterm.home_dir, "default")
+
+          wezterm.mux.set_active_workspace("default")
+        end)
+
         -- Key bindings
         config.disable_default_key_bindings = true
         config.keys = {
+          {
+            key = "q",
+            mods = "CMD|SHIFT",
+            action = act.QuitApplication,
+          },
           {
             key = "n",
             mods = "CTRL|SHIFT",
@@ -122,7 +212,7 @@ in
           {
             key = "t",
             mods = "CTRL|SHIFT",
-            action = act.SpawnTab("DefaultDomain"),
+            action = act.SpawnTab("CurrentPaneDomain"),
           },
           {
             key = "w",
@@ -150,6 +240,28 @@ in
             action = act.MoveTabRelative(1),
           },
           {
+            key = "<",
+            mods = "CTRL|SHIFT",
+            action = act.PromptInputLine({
+              description = "Rename Tab",
+              action = wezterm.action_callback(function(window, pane, line)
+                if line then
+                  window:active_tab():set_title(line)
+                end
+              end),
+            }),
+          },
+          {
+            key = "s",
+            mods = "LEADER",
+            action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+          },
+          {
+            key = "s",
+            mods = "LEADER|CTRL",
+            action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+          },
+          {
             key = "m",
             mods = "CTRL|SHIFT",
             action = act.TogglePaneZoomState,
@@ -164,11 +276,6 @@ in
             mods = "SHIFT",
             action = act.ScrollByPage(1),
           },
-          -- {
-          --   key = "r",
-          --   mods = "CTRL|SHIFT",
-          --   action = act.ReloadConfiguration,
-          -- },
           {
             key = "p",
             mods = "CTRL|SHIFT",
@@ -255,28 +362,6 @@ in
               key = "a",
             }),
           },
-
-          -- Plugin keybindings
-          {
-            key = "s",
-            mods = "LEADER",
-            action = ws_mgr.apply_workspace(),
-          },
-          {
-            key = "s",
-            mods = "LEADER|CTRL",
-            action = ws_mgr.apply_workspace(),
-          },
-          {
-            key = "s",
-            mods = "LEADER|SHIFT",
-            action = ws_mgr.save_workspace(),
-          },
-          {
-            key = "d",
-            mods = "LEADER|SHIFT",
-            action = ws_mgr.delete_workspace(),
-          },
           -- Rename current workspace
           {
             key = "$",
@@ -292,7 +377,7 @@ in
           },
           -- Prompt for a name to use for a new workspace and switch to it.
           {
-            key = "n",
+            key = "s",
             mods = "LEADER|SHIFT",
             action = act.PromptInputLine({
               description = wezterm.format({
