7ff13a2d92a560692b71768c0ea3d4940727eb4a
Tobias Lindholm <tobias.lindholm@antob.se>
2026-08-09 11:14:14 +0200

    chore: refactor hyllan backups

--- hosts/hyllan/backup.nix
+++ hosts/hyllan/backup.nix
@@ -10,88 +10,90 @@ let
   baseMountPoint = "/mnt/restic-snapshot";
   baseRepo = "s3:hel1.your-objectstorage.com/antob-hyllan-backups";
 
-  mkResticBackup =
-    {
-      dataset,
-      paths ? [ "" ],
-      excludes ? [ ],
-    }:
-    {
-      initialize = true;
-      environmentFile = secrets.restic_env_file.path;
-      repository = "${baseRepo}/${dataset}";
+  mkResticBackups =
+    backups:
+    builtins.listToAttrs (
+      map (
+        {
+          name,
+          dataset ? name,
+          paths ? [ "" ],
+          excludes ? [ ],
+        }:
+        {
+          inherit name;
+          value = {
+            initialize = true;
+            environmentFile = secrets.restic_env_file.path;
+            repository = "${baseRepo}/${dataset}";
 
-      paths = map (path: "${baseMountPoint}/${dataset}${path}") paths;
-      exclude = map (exclude: "${baseMountPoint}/${dataset}${exclude}") excludes;
+            paths = map (path: "${baseMountPoint}/${dataset}${path}") paths;
+            exclude = map (exclude: "${baseMountPoint}/${dataset}${exclude}") excludes;
 
-      backupPrepareCommand = ''
-        ${pkgs.zfs}/bin/zfs snapshot zpool/${dataset}@restic-backup
-        mkdir -p ${baseMountPoint}/${dataset}
-        ${pkgs.util-linux}/bin/mount -t zfs zpool/${dataset}@restic-backup ${baseMountPoint}/${dataset}
-      '';
+            backupPrepareCommand = ''
+              ${pkgs.zfs}/bin/zfs snapshot zpool/${dataset}@restic-backup
+              mkdir -p ${baseMountPoint}/${dataset}
+              ${pkgs.util-linux}/bin/mount -t zfs zpool/${dataset}@restic-backup ${baseMountPoint}/${dataset}
+            '';
 
-      backupCleanupCommand = ''
-        ${pkgs.util-linux}/bin/umount ${baseMountPoint}/${dataset}
-        ${pkgs.zfs}/bin/zfs destroy zpool/${dataset}@restic-backup
-        rmdir ${baseMountPoint}/${dataset}
-      '';
+            backupCleanupCommand = ''
+              ${pkgs.util-linux}/bin/umount ${baseMountPoint}/${dataset}
+              ${pkgs.zfs}/bin/zfs destroy zpool/${dataset}@restic-backup
+              rmdir ${baseMountPoint}/${dataset}
+            '';
 
-      timerConfig = {
-        OnCalendar = "01:05";
-        Persistent = true;
-        RandomizedDelaySec = "30m";
-      };
+            timerConfig = {
+              OnCalendar = "01:05";
+              Persistent = true;
+              RandomizedDelaySec = "30m";
+            };
 
-      pruneOpts = [
-        "--keep-daily 7"
-        "--keep-weekly 4"
-        "--keep-monthly 12"
-      ];
-    };
+            pruneOpts = [
+              "--keep-daily 7"
+              "--keep-weekly 4"
+              "--keep-monthly 12"
+            ];
+          };
+        }
+      ) backups
+    );
 in
 {
-  services.restic.backups = {
-    hass = mkResticBackup {
-      dataset = "hass";
-    };
-
-    photoprism = mkResticBackup {
-      dataset = "photoprism";
-    };
-
-    mysql = mkResticBackup {
-      dataset = "mysql";
-    };
-
-    postgresql = mkResticBackup {
-      dataset = "postgresql";
-    };
-
-    public_share = mkResticBackup {
-      dataset = "public_share";
-    };
-
-    private_share = mkResticBackup {
-      dataset = "private_share";
-    };
-
-    syncthing = mkResticBackup {
-      dataset = "syncthing";
+  services.restic.backups = mkResticBackups [
+    {
+      name = "hass";
+    }
+    {
+      name = "photoprism";
+    }
+    {
+      name = "mysql";
+    }
+    {
+      name = "postgresql";
+    }
+    {
+      name = "public_share";
+    }
+    {
+      name = "private_share";
+    }
+    {
+      name = "syncthing";
       excludes = [
         "/services/syncthing/Projects"
       ];
-    };
-
-    tank = mkResticBackup {
-      dataset = "tank";
+    }
+    {
+      name = "tank";
       paths = [
         "/services/vaultwarden"
         "/services/mass"
         "/services/caddy"
         "/services/esphome"
       ];
-    };
-  };
+    }
+  ];
 
   # The restic module hardcodes PrivateTmp=true, which hides the ZFS mount
   # from ExecStart. Disable it so the snapshot mount survives into the backup.
