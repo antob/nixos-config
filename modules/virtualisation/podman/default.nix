c916ee4ad04fb2cb5fec4262b944efc6c092595a
Tobias Lindholm <tobias.lindholm@antob.se>
2026-09-05 14:19:02 +0200

    fix: add missing podman change

--- modules/virtualisation/podman/default.nix
+++ modules/virtualisation/podman/default.nix
@@ -18,11 +18,23 @@ in
   config = mkIf cfg.enable {
     virtualisation.podman = {
       enable = true;
+      autoPrune.enable = true;
       dockerCompat = true;
       dockerSocket.enable = true;
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
+
+    # Enable container name DNS for all Podman networks.
+    networking.firewall.interfaces =
+      let
+        matchAll = if !config.networking.nftables.enable then "podman+" else "podman*";
+      in
+      {
+        "${matchAll}".allowedUDPPorts = [ 53 ];
+      };
+
+    virtualisation.oci-containers.backend = "podman";
 
     virtualisation.containers.storage.settings = {
       storage = {
