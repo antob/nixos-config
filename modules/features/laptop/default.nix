e6fdd32e92dd6befd2f051736a1750cf6be740fe
Tobias Lindholm <tobias.lindholm@antob.se>
2026-09-05 11:09:00 +0200

    feat: switch ppd profiles automatically. enable iwlwifi power save

--- modules/features/laptop/default.nix
+++ modules/features/laptop/default.nix
@@ -35,15 +35,45 @@ in
       powertop.enable = true;
     };
 
+    boot.extraModprobeConfig = ''
+      options iwlwifi power_save=1
+    '';
+
     services = {
       # Power optimizer daemons. Choose one.
       power-profiles-daemon.enable = true;
       tlp.enable = false;
 
+      # Switch power-profiles-daemon profile when AC adapter changes state.
+      udev.extraRules = ''
+        ACTION=="add|change", SUBSYSTEM=="power_supply", KERNEL=="AC*", ENV{POWER_SUPPLY_ONLINE}=="0", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
+        ACTION=="add|change", SUBSYSTEM=="power_supply", KERNEL=="AC*", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced"
+      '';
+
       logind.settings.Login = {
         HandleLidSwitch = "suspend-then-hibernate";
         HandleLidSwitchExternalPower = "suspend";
       };
+    };
+
+    # Set the PPD profile at boot, once the daemon is available.
+    systemd.services.ppd-ac-profile = {
+      description = "Set power-profiles-daemon profile based on AC state";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "power-profiles-daemon.service" ];
+      requires = [ "power-profiles-daemon.service" ];
+      serviceConfig.Type = "oneshot";
+      script = ''
+        ac_online=0
+        for ps in /sys/class/power_supply/*; do
+          [ "$(cat $ps/type)" = "Mains" ] && [ "$(cat $ps/online)" = "1" ] && ac_online=1
+        done
+        if [ "$ac_online" = "1" ]; then
+          ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
+        else
+          ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
+        fi
+      '';
     };
   };
 }
